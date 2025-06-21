d606aed2b8264f5a4b42eaa3423412d7da2067d6
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-06-21 09:48:54 -0300

    Chess layer and printing the board

---
+++ src/main/java/com/gabriel/chess/ChessPiece.java
@@ -0,0 +1,18 @@
+package com.gabriel.chess;
+
+import com.gabriel.boardgame.Board;
+import com.gabriel.boardgame.Piece;
+
+public class ChessPiece extends Piece {
+
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/com/gabriel/chess/Color.java
@@ -0,0 +1,7 @@
+package com.gabriel.chess;
+
+public enum Color {
+
+    BLACK,
+    WHITE;
+}
