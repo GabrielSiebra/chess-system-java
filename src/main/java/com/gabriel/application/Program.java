2f1a0fed7c8968a4aa72d0b67c2c84f2fb2b5b8c
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-06-25 21:33:40 -0300

    Moving Pieces

--- src/main/java/com/gabriel/application/Program.java
+++ src/main/java/com/gabriel/application/Program.java
@@ -3,15 +3,30 @@ package com.gabriel.application;
 import com.gabriel.boardgame.Board;
 import com.gabriel.boardgame.Position;
 import com.gabriel.chess.ChessMatch;
+import com.gabriel.chess.ChessPiece;
+import com.gabriel.chess.ChessPosition;
+
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
 
+        while(true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
 
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
